5c9ac09e711c00863010543d48804ce07a4b5356
andy3790 <parkyj477@gmail.com>
2021-12-14 23:43:23 +0900

    프로그램 이름 변경 및 아이콘 추가

--- 3D Interactive Program Project/3D Interactive Program Project/main.cpp
+++ 3D Interactive Program Project/3D Interactive Program Project/main.cpp
@@ -99,7 +99,7 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH); // 디스플레이 모드 설정
 	glutInitWindowPosition(100, 100); // 윈도우의 위치 지정
 	glutInitWindowSize(WIN_WIDTH, WIN_HIGHT); // 윈도우의 크기 지정
-	glutCreateWindow("Shader Programing"); // 윈도우 생성 (윈도우 이름)
+	glutCreateWindow("Cube"); // 윈도우 생성 (윈도우 이름)
 		//--- GLEW 초기화하기
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) // glew 초기화
